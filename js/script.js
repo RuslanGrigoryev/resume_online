a39f116e25e5f3a6da235448846f3fb601ecd43f
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-06-06 16:25:01 +0300

    Animate skills

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-﻿
+﻿$(function(){
+  
+  var h = $('.skill_header');
+
+  $(window).scroll(function() {
+	var scrollTop     = $(window).scrollTop(),
+	    elementOffset = h.offset().top,
+	    distance      = (elementOffset - scrollTop);
+		if (distance < 300) {
+			htmlSkill.init();
+			cssSkill.init();
+			preProcessorSkill.init();
+			CssGrid.init();
+			jsSkill.init();
+			gitSkill.init();
+			cmsSkill.init();
+			emailSkill.init();
+			adobeSkill.init();
+		}
+  });
+
+
+});
