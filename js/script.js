b5f53813eb61382ac4a1fb6b92ca0cc767662771
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-07-01 14:55:58 +0300

    Added portfolio markup

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
 ﻿$(function(){
   
-  var h = $('.skill_header');
+  if ($('.skill_header').length) {
+	  var h = $('.skill_header');
 
-  $(window).scroll(function() {
-	var scrollTop     = $(window).scrollTop(),
-	    elementOffset = h.offset().top,
-	    distance      = (elementOffset - scrollTop);
-		if (distance < 300) {
-			htmlSkill.init();
-			cssSkill.init();
-			preProcessorSkill.init();
-			CssGrid.init();
-			jsSkill.init();
-			gitSkill.init();
-			cmsSkill.init();
-			emailSkill.init();
-			adobeSkill.init();
-		}
-  });
+	  $(window).scroll(function() {
+		var scrollTop     = $(window).scrollTop(),
+		    elementOffset = h.offset().top,
+		    distance      = (elementOffset - scrollTop);
+			if (distance < 300) {
+				htmlSkill.init();
+				cssSkill.init();
+				preProcessorSkill.init();
+				CssGrid.init();
+				jsSkill.init();
+				gitSkill.init();
+				cmsSkill.init();
+				emailSkill.init();
+				adobeSkill.init();
+			}
+	  });
+  }
 
 
 });
